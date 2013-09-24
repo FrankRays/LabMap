ce219b4762240e9c2c69e9ad82d154373bb4857b
Sai Phaninder Reddy Jonnala <jspreddy@yahoo.com>
2013-09-23 21:00:14 -0400

    -

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'db-devel1';
-$db['default']['username'] = 'event-reg-adm';
-$db['default']['password'] = 'sIGNUPMENow2!';
-$db['default']['database'] = 'event-reg';
+$db['default']['hostname'] = '';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
